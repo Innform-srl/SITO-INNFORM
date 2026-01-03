90e537683279cd25e59c718526f2e5c175e3ce5f
fralias <fralias@gmail.com>
2026-01-03 17:31:50 +0100

    Add Favicon component with SVG gradient icon

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,7 @@ import { CookiePolicy } from './components/CookiePolicy';
 import { FAQPage } from './components/FAQPage';
 
 import { ScrollToTop } from './components/ScrollToTop';
+import { Favicon } from './components/Favicon';
 
 function SkipLinks() {
   return (
@@ -77,6 +78,7 @@ function HomePage() {
 export default function App() {
   return (
     <Router>
+      <Favicon />
       <ScrollToTop />
       <div className="min-h-screen bg-white">
         <Routes>
